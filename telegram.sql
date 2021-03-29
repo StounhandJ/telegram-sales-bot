5b5fe197d3127e4c639d499b04d7df9447754de6
StounhandJ <rmarinicev@gmail.com>
2021-03-29 16:53:56 +0300

    update sql

--- telegram.sql
+++ telegram.sql
@@ -9,100 +9,104 @@ SET time_zone = "+00:00";
 
 
 CREATE TABLE `ban_list` (
-  `id` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `date` int(11) NOT NULL
+  `id` int NOT NULL,
+  `userID` int NOT NULL,
+  `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `department` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `staff` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `tag` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL,
+  `userID` int NOT NULL,
+  `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `document` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `isOrder` tinyint(1) NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `date` int(11) NOT NULL
+  `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL,
+  `userID` int NOT NULL,
+  `price` int NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `document` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `active` tinyint(1) NOT NULL,
   `separate_payment` tinyint(1) NOT NULL,
-  `payment_key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `date` int(11) NOT NULL
+  `payment_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `orders_processing` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `userID` int(10) UNSIGNED NOT NULL,
-  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int UNSIGNED NOT NULL,
+  `userID` int UNSIGNED NOT NULL,
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `document` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `active` tinyint(1) NOT NULL,
   `separate_payment` tinyint(1) NOT NULL,
   `percent` tinyint(1) NOT NULL,
-  `discount` int(10) UNSIGNED NOT NULL,
-  `date` int(10) UNSIGNED NOT NULL
+  `discount` int UNSIGNED NOT NULL,
+  `date` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `payment` (
-  `id` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL,
+  `userID` int NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `document` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `separate_payment` tinyint(1) NOT NULL,
   `additional` tinyint(1) NOT NULL,
-  `price` int(11) NOT NULL,
-  `secret_key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` int(11) NOT NULL
+  `price` int NOT NULL,
+  `secret_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `payment_history` (
-  `id` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `date` int(11) NOT NULL
+  `id` int NOT NULL,
+  `userID` int NOT NULL,
+  `amount` int NOT NULL,
+  `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `promo_codes` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `percent` tinyint(1) NOT NULL,
-  `discount` int(10) UNSIGNED NOT NULL,
+  `discount` int UNSIGNED NOT NULL,
   `limitation_use` tinyint(1) NOT NULL,
-  `count` int(7) NOT NULL
+  `count` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `orderID` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `orderID` int NOT NULL,
   `staff` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `departmentTag` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `departmentTag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `tasks_completes` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `userID` int(10) UNSIGNED NOT NULL,
-  `orderID` int(10) UNSIGNED NOT NULL,
-  `departmentTag` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int UNSIGNED NOT NULL,
+  `userID` int UNSIGNED NOT NULL,
+  `orderID` int UNSIGNED NOT NULL,
+  `departmentTag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `document` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `date` int(10) UNSIGNED NOT NULL
+  `date` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
+CREATE TABLE `user_information` (
+  `userID` int NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `payment` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 ALTER TABLE `ban_list`
@@ -141,34 +145,37 @@ ALTER TABLE `user_information`
 
 
 ALTER TABLE `ban_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `messages_coupon`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `orders_processing`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `payment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `payment_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `promo_codes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `tasks_completes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
